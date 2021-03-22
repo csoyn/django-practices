02c24085f1f4e8709d6e7688140ed52050ca28fd
tony <edutony.hphk@gmail.com>
2021-03-22 18:17:30 +0900

    2021-03-22 practice

--- 05.django_auth/accounts/views.py
+++ 05.django_auth/accounts/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.http import require_POST
 
+# DB 유저 세션 생성(CREATE)
 def login(request):
+    # 로그인되어 있다면
+    # 로그인 페이지를 볼 필요도 없고 실제 로그인 동작도 필요없다.
+    # print(dir(request.user))
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('articles:index')
+            # next 파라미터는 여기서 사용!
+            # ?next=/articles/create/
+            # A or B : 단축평가를 활용
+            # A : request.GET.get('next') 가 있는 경우 B를 확인하지 않고 A로 이동
+            # A가 없는 경우에는 B로 간다
+            return redirect(request.GET.get('next') or 'articles:index')
     else:
         # 개발자가 생성한 form class 사용 X 
         # => 장고가 제공하는 form class 사용 O
@@ -17,3 +31,36 @@ def login(request):
         'form': form
     }
     return render(request, 'accounts/login.html', context)
+
+
+# DB 유저 세션 삭제(DELETE)
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+
+# DB 유저 정보 생성(CREATE)
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # 회원 가입 완료
+            user = form.save()
+            # 로그인
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+# DB 유저 정보 삭제(DELETE)
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+    return redirect('accounts:login')
--- 05.django_auth/articles/views.py
+++ 05.django_auth/articles/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.decorators import login_required
 from .models import Article
 from .forms import ArticleForm
 
 def index(request):
-    # articles = Article.objects.all()
     articles = Article.objects.order_by('-pk')
     context = {
         'articles': articles
     }
     return render(request, 'articles/index.html', context)
 
-def detail(request, pk):
-    # article = Article.objects.get(pk=pk)
-    # pk가 없었을 때 404
-    article = get_object_or_404(Article, pk=pk)
 
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
     context = {
         'article': article
     }
     return render(request, 'articles/detail.html', context)
 
-
+# login_required를 거치면 login페이지로 리다이렉트 되고,
+# 다음과 같은 url이 생성됩니다.
+# ~/accounts/login/?next=/articles/create/
+# 파라미터!
+@login_required
+@require_http_methods(['GET', 'POST'])
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST, request.FILES)
@@ -29,8 +33,44 @@ def create(request):
             return redirect('articles:detail', article.pk)
     else:
         form = ArticleForm()
-
     context = {
         'form': form
     }
     return render(request, 'articles/create.html', context)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
+
+
+# login required 삭제!
+@require_POST
+def delete(request, pk):
+    if request.user.is_authenticated:
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
+    return redirect('articles:index')
+
+
+# 로그인에 성공했을 때 next 동작으로 delete 하게되면 : 405
+# => require_POST (redirect 동작이 GET 동작이라서!)
+# @login_required
+# @require_POST
+# def delete(request, pk):
+#     article = get_object_or_404(Article, pk=pk)
+#     article.delete()
+#     return redirect('articles:index')
